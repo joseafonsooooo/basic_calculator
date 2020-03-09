1ab0f42cfa31307929eedad41f0217a098d30aba
J <jrafonso@worten.pt>
2020-03-09 12:21:00 +0000

    Calc front-end

---
+++ script.js
@@ -0,0 +1,9 @@
+var buttons = document.getElementsByClassName("keys");
+
+
+document.getElementsByClassName("key7").onclick = function(){
+    document.getElementsByClassName("display").innerHTML = "7";
+};
+
+
+
